f05f194b889f5ca7562116fc2c33f7943edaad29
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2020-11-13 15:37:32 +0100

    Release 1.26.7 (#44)

--- src/kubernetes.go
+++ src/kubernetes.go
@@ -70,7 +70,7 @@ const (
 	defaultDiscoveryCacheTTL           = time.Hour
 
 	integrationName    = "com.newrelic.kubernetes"
-	integrationVersion = "1.26.6"
+	integrationVersion = "1.26.7"
 	nodeNameEnvVar     = "NRK8S_NODE_NAME"
 )
 
