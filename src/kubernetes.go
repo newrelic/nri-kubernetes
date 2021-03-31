d1681ad2a46ed01f20fb26e55af32cc13d614cbd
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2021-03-31 11:48:25 +0200

    core: update version to 2.3.1 (#82)

--- src/kubernetes.go
+++ src/kubernetes.go
@@ -70,7 +70,7 @@ const (
 	defaultDiscoveryCacheTTL           = time.Hour
 
 	integrationName    = "com.newrelic.kubernetes"
-	integrationVersion = "2.3.0"
+	integrationVersion = "2.3.1"
 	nodeNameEnvVar     = "NRK8S_NODE_NAME"
 )
 
