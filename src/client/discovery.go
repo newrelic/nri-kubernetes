63d46049c2accfccf955dc893138ad67ad9730d5
Mateusz Gozdek <mgozdek@microsoft.com>
2021-10-14 16:20:29 +0200

    src/client: decompose HTTPClient interface into multiple types
    To start process of phasing out NodeIP() method, so clients which do not
    need it can start only using HTTPDoer instead of HTTPClient.
    
    This is because NodeIP() method in HTTPClient does not make any sense.
    
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- src/client/discovery.go
+++ src/client/discovery.go
@@ -19,6 +19,17 @@ type MultiDiscoverer interface {
 
 // HTTPClient allows to connect to the discovered Kubernetes services
 type HTTPClient interface {
+	HTTPDoer
+	NodeIPGetter
+}
+
+// HTTPDoer is an interface for HTTP client with, which should provide
+// scheme, port and hostname for the HTTP call.
+type HTTPDoer interface {
 	Do(method, path string) (*http.Response, error)
+}
+
+// NodeIPGetter allows getting discovered Node IP.
+type NodeIPGetter interface {
 	NodeIP() string
 }
