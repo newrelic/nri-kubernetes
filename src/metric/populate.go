68e8ad020d6091856fbaadccd34755ab77c74f34
Mateusz Gozdek <mgozdek@microsoft.com>
2021-10-25 10:34:40 +0200

    src/metric: remove unused MultipleErrs type
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- src/metric/populate.go
+++ src/metric/populate.go
@@ -2,7 +2,6 @@ package metric
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/newrelic/infra-integrations-sdk/integration"
 	"k8s.io/apimachinery/pkg/version"
@@ -10,23 +9,6 @@ import (
 	"github.com/newrelic/nri-kubernetes/v2/src/data"
 	"github.com/newrelic/nri-kubernetes/v2/src/definition"
 )
-
-// MultipleErrs represents a bunch of errs.
-// Recoverable == true means that you can keep working with those errors.
-// Recoverable == false means you must handle the errors or panic.
-type MultipleErrs struct {
-	Errs []error
-}
-
-// Error implements error interface
-func (e MultipleErrs) Error() string {
-	s := "multiple errors:"
-
-	for _, err := range e.Errs {
-		s = fmt.Sprintf("%s\n%s", s, err)
-	}
-	return s
-}
 
 // Populate populates k8s raw data to sdk metrics.
 func Populate(
