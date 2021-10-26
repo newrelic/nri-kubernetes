07a747533478192aad59ca116ebfb98a6bfc6d47
Mateusz Gozdek <mgozdek@microsoft.com>
2021-10-26 14:24:20 +0200

    src/metric: remove redundant Substract test
    There is almost the same TestSubtract test already.
    
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- src/metric/metric_test.go
+++ src/metric/metric_test.go
@@ -4,8 +4,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/newrelic/nri-kubernetes/v2/src/definition"
 )
 
 func TestK8sMetricSetTypeGuesser(t *testing.T) {
@@ -25,21 +23,4 @@ func TestK8sMetricSetTypeGuesser(t *testing.T) {
 			assert.Equal(t, testCase.expected, guess)
 		})
 	}
-}
-
-func TestSubtractorFunc(t *testing.T) {
-	// given 2 FetchFunc
-	var left definition.FetchFunc
-	var right definition.FetchFunc
-	left = func(groupLabel, entityID string, groups definition.RawGroups) (definition.FetchedValue, error) {
-		return float64(10), nil
-	}
-	right = func(groupLabel, entityID string, groups definition.RawGroups) (definition.FetchedValue, error) {
-		return float64(5), nil
-	}
-
-	sub := Subtract(left, right)
-	result, err := sub("", "", nil)
-	assert.NoError(t, err)
-	assert.Equal(t, float64(5), result)
 }
