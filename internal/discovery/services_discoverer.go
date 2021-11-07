1ca4bf8f33a068860ab221f6ac5c4c63c9f427fa
paologallinaharbur <paologallina1992@gmail.com>
2021-11-07 22:45:25 +0100

    feat(services): watches instead of list

---
+++ internal/discovery/services_discoverer.go
@@ -0,0 +1,52 @@
+package discovery
+
+import (
+	"fmt"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"time"
+)
+
+type ServicesLister interface {
+	List(selector labels.Selector) (ret []*corev1.Service, err error)
+}
+
+type servicesDiscoverer struct {
+	ServicesLister ServicesLister
+}
+
+type ServiceDiscoverer interface {
+	Discover() ([]*corev1.Service, error)
+}
+
+func (d *servicesDiscoverer) Discover() ([]*corev1.Service, error) {
+
+	services, err := d.ServicesLister.List(labels.Everything())
+	if err != nil {
+		return nil, fmt.Errorf("listing services: %w", err)
+	}
+
+	return services, nil
+}
+
+func NewServicesDiscoverer(client kubernetes.Interface) (ServiceDiscoverer, error) {
+	// Arbitrary value, same used in Prometheus.
+	resyncDuration := 10 * time.Minute
+	stopCh := make(chan struct{})
+	sl := func(options ...informers.SharedInformerOption) ServicesLister {
+		factory := informers.NewSharedInformerFactoryWithOptions(client, resyncDuration, options...)
+
+		lister := factory.Core().V1().Services().Lister()
+
+		factory.Start(stopCh)
+		factory.WaitForCacheSync(stopCh)
+
+		return lister
+	}
+
+	return &servicesDiscoverer{
+		ServicesLister: sl(),
+	}, nil
+}
