11f29a4666b56effb9f5945f8fa653a059d7d2d2
Alvaro Cabanas <acabanas@newrelic.com>
2021-11-15 14:39:59 +0100

    Config test and structure added
    Signed-off-by: Alvaro Cabanas <acabanas@newrelic.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,121 +1,137 @@
 package config
 
 import (
-	"os"
 	"strings"
 	"time"
 
-	"github.com/newrelic/infra-integrations-sdk/log"
-	"github.com/octago/sflags"
-	"github.com/octago/sflags/gen/gpflag"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
+const (
+	FileName = "nri-kubernetes"
+	FilePath = "/etc/newrelic-infra/integrations.d/"
+)
+
 type Config struct {
-	Verbose     bool          `mapstructure:"verbose"`
-	ClusterName string        `mapstructure:"cluster_name"`
-	Interval    time.Duration `mapstructure:"interval"`
+	Verbose          bool          `mapstructure:"verbose"`
+	ClusterName      string        `mapstructure:"cluster_name"`
+	Interval         time.Duration `mapstructure:"interval"`
+	Timeout          time.Duration `mapstructure:"timeout"`
+	CacheDir         string        `mapstructure:"cache_dir"`
+	NetworkRouteFile string        `mapstructure:"network_route_file"`
 
-	KSM       `mapstructure:"ksm"`
-	Kubelet   `mapstructure:"kubelet"`
-	Etcd      `mapstructure:"etcd"`
-	ApiServer `mapstructure:"api_server"`
-
-	Logger log.Logger
+	Kubelet           `mapstructure:"kubelet"`
+	Etcd              `mapstructure:"etcd"`
+	Scheduler         `mapstructure:"scheduler"`
+	ControllerManager `mapstructure:"controller_manager"`
+	ApiServer         `mapstructure:"api_server"`
+	KSM               `mapstructure:"ksm"`
 }
 
 type Kubelet struct {
+	Discovery struct {
+		Static struct {
+			URL string
+		} `mapstructure:"static"`
+		ClientAuthentication struct {
+			CAFile      string `mapstructure:"ca_file"`
+			BearerToken string `mapstructure:"bearer_token"`
+		} `mapstructure:"client_authentication"`
+	} `mapstructure:"discovery"`
 }
 
 type Etcd struct {
+	Enable struct {
+		NodeSelector string `mapstructure:"node_selector"`
+	} `mapstructure:"enable"`
+	Discovery struct {
+		Static struct {
+			URLs []string `mapstructure:"urls"`
+		} `mapstructure:"static"`
+		LocalPod struct {
+			LabelSelectors []string `mapstructure:"label_selectors"`
+		} `mapstructure:"local_pod"`
+	} `mapstructure:"discovery"`
+	Secret struct {
+		Name      string `mapstructure:"name"`
+		Namespace string `mapstructure:"namespace"`
+	} `mapstructure:"secret"`
+}
+
+type Scheduler struct {
+	Discovery struct {
+		Static struct {
+			URL string `mapstructure:"url"`
+		} `mapstructure:"static"`
+		LocalPod struct {
+			LabelSelectors []string `mapstructure:"label_selectors"`
+		} `mapstructure:"local_pod"`
+	} `mapstructure:"discovery"`
+}
+
+type ControllerManager struct {
+	Discovery struct {
+		Static struct {
+			URL string `mapstructure:"url"`
+		} `mapstructure:"static"`
+		LocalPod struct {
+			LabelSelectors []string `mapstructure:"label_selectors"`
+		} `mapstructure:"local_pod"`
+	} `mapstructure:"discovery"`
 }
 
 type ApiServer struct {
+	Discovery struct {
+		Static struct {
+			URL string `mapstructure:"url"`
+		} `mapstructure:"static"`
+		LocalPod struct {
+			LabelSelectors []string `mapstructure:"label_selectors"`
+		} `mapstructure:"local_pod"`
+	} `mapstructure:"discovery"`
+	Cache struct {
+		TTL                 string `mapstructure:"ttl"`
+		TTL_jitter          int    `mapstructure:"ttl_jitter"`
+		K8sVersionTTL       string `mapstructure:"k8s_version_ttl"`
+		K8sVersionTTLJitter int    `mapstructure:"k8s_version_ttl_jitter"`
+	} `mapstructure:"cache"`
+	SecurePort int `mapstructure:"secure_port"`
 }
 
 type KSM struct {
-	// URL defines a static endpoint for KSM.
-	StaticEndpoint string `mapstructure:"static_endpoint"`
-
-	// Autodiscovery settings.
-	// Scheme that will be used to hit the endpoints of discovered KSM services. Defaults to http.
-	Scheme string `mapstructure:"scheme"`
-	// If set, Port will discard all endpoints discovered that do not use this specified port. Otherwise, all endpoints will be considered.
-	Port int `mapstructure:"port"`
-	// PodLabel is the selector used to filter Endpoints.
-	PodLabel string `mapstructure:"pod_label"`
-	// Namespace can be used to restric the search to a particular namespace.
-	Namespace string `mapstructure:"namespace"`
-	// If set, Distributed will instruct the integration to scrape all KSM endpoints rather than just the first one.
-	Distributed bool `mapstructure:"distributed"`
+	Discovery struct {
+		// Scheme that will be used to hit the endpoints of discovered KSM services. Defaults to http.
+		Scheme string `mapstructure:"scheme"`
+		// If set, Port will discard all endpoints discovered that do not use this specified port. Otherwise, all endpoints will be considered.
+		Port int `mapstructure:"port"`
+		// If set, Distributed will instruct the integration to scrape all KSM endpoints rather than just the first one.
+		Distributed bool `mapstructure:"distributed"`
+		Static      struct {
+			URL string `mapstructure:"url"`
+		} `mapstructure:"static"`
+		Endpoints struct {
+			LabelSelector string `mapstructure:"label_selector"`
+			Namespace     string `mapstructure:"namespace"`
+		} `mapstructure:"endpoints"`
+	} `mapstructure:"discovery"`
 }
 
-func LoadConfig() (Config, error) {
+func LoadConfig(configPath, configName string) (Config, error) {
 	var cfg Config
 
 	// Env Variables
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	// Flags
-	flagSet := pflag.NewFlagSet("server_a", pflag.ContinueOnError)
-	if err := gpflag.ParseTo(&cfg, flagSet, sflags.FlagDivider("."), sflags.FlagTag("mapstructure")); err != nil {
-		return Config{}, err
-	}
-	flagSet.Parse(os.Args[1:])
-	if err := viper.BindPFlags(flagSet); err != nil {
-		return Config{}, err
-	}
-
 	// Config File
-	viper.SetConfigName("server")
-	viper.AddConfigPath("testdata")
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(configPath)
 
-	var isFileRead bool
-	if err := viper.ReadInConfig(); err == nil {
-		isFileRead = true
-	}
+	// If error reading file or file not found, use flag/env variables
+	_ = viper.ReadInConfig()
 	if err := viper.Unmarshal(&cfg); err != nil {
 		return Config{}, err
 	}
 
-	cfg.Logger = log.NewStdErr(cfg.Verbose)
-	if isFileRead {
-		cfg.Logger.Infof("Using config file: %s \n", viper.ConfigFileUsed())
-	}
-
 	return cfg, nil
 }
-
-/*
-func LoadConfig() Config {
-	// strconv.ParseBool(os.Getenv("VERBOSE"))
-	kubeStateMetricsPort, _ := strconv.Atoi(os.Getenv("KUBE_STATE_METRIC_PORT"))
-	distributedKubeStateMetrics, _ := strconv.ParseBool(os.Getenv("DISTRIBUTED_KUBE_STATE_METRIC"))
-	schema := "http"
-
-	if os.Getenv("KUBE_STATE_METRIC_SCHEME") != "" {
-		schema = os.Getenv("KUBE_STATE_METRIC_SCHEME")
-	}
-
-	var ksmURL string
-	if u, err := url.Parse(os.Getenv("KUBE_STATE_METRIC_URL")); err != nil {
-		ksmURL = net.JoinHostPort(u.Host, u.Port())
-		schema = u.Scheme
-	}
-
-	return Config{
-		ClusterName: os.Getenv("CLUSTER_NAME"),
-		Verbose:     true,
-		Interval:    15 * time.Second,
-		KSM: KSM{
-			StaticEndpoint: ksmURL,
-			PodLabel:       os.Getenv("KUBE_STATE_METRIC_POD_LABEL"),
-			Scheme:         schema,
-			Port:           kubeStateMetricsPort,
-			Namespace:      os.Getenv("KUBE_STATE_METRIC_NAMESPACE"),
-			Distributed:    distributedKubeStateMetrics,
-		},
-	}
-}*/
