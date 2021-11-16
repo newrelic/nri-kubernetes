3521155f1de92ee30ccfc56151a23eb43956bd72
paologallinaharbur <paologallina1992@gmail.com>
2021-11-16 15:42:05 +0100

    feat(main): updated main to run deprecated code

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,11 @@ import (
 // This is only a mock of the new config
 type Mock struct {
 	KSM
+	ControlPlane
+	Kubelet
 	Verbose     bool
+	NodeName    string
+	Timeout     time.Duration
 	ClusterName string
 	Interval    time.Duration
 }
@@ -31,6 +35,39 @@ type KSM struct {
 	Namespace string
 	// If set, Distributed will instruct the integration to scrape all KSM endpoints rather than just the first one.
 	Distributed bool
+
+	Enabled bool
+}
+
+type ControlPlane struct {
+	ETCD              ETCD
+	ApiServer         APIServer
+	Scheduler         Scheduler
+	ControllerManager ControllerManager
+	Enabled           bool
+}
+
+type APIServer struct {
+	APIServerEndpointURL string
+	APIServerSecurePort  string
+}
+
+type ETCD struct {
+	EtcdEndpointURL        string
+	EtcdTLSSecretNamespace string
+	EtcdTLSSecretName      string
+}
+
+type Scheduler struct {
+	SchedulerEndpointURL string
+}
+
+type ControllerManager struct {
+	ControllerManagerEndpointURL string
+}
+
+type Kubelet struct {
+	Enabled bool
 }
 
 func LoadConfig() Mock {
@@ -49,17 +86,46 @@ func LoadConfig() Mock {
 		schema = u.Scheme
 	}
 
+	ksmEnabled, _ := strconv.ParseBool(os.Getenv("KUBE_STATE_METRIC_ENABLED"))
+	kubeleEnabled, _ := strconv.ParseBool(os.Getenv("CONTROL_PLANE_ENABLED"))
+	controlPlanEnabled, _ := strconv.ParseBool(os.Getenv("KUBELET_ENABLED"))
+
 	return Mock{
 		ClusterName: os.Getenv("CLUSTER_NAME"),
 		Verbose:     true,
+		Timeout:     time.Millisecond * 5000,
 		Interval:    15 * time.Second,
+		NodeName:    os.Getenv("NRK8S_NODE_NAME"),
+		Kubelet: Kubelet{
+			Enabled: kubeleEnabled,
+		},
 		KSM: KSM{
+			Enabled: ksmEnabled,
+
 			StaticURL:   ksmURL,
 			PodLabel:    os.Getenv("KUBE_STATE_METRIC_POD_LABEL"),
 			Scheme:      schema,
 			Port:        kubeStateMetricsPort,
 			Namespace:   os.Getenv("KUBE_STATE_METRIC_NAMESPACE"),
 			Distributed: distributedKubeStateMetrics,
+		},
+		ControlPlane: ControlPlane{
+			Enabled: controlPlanEnabled,
+			ETCD: ETCD{
+				EtcdEndpointURL:        os.Getenv("ETCD_ENDPOINT_URL"),
+				EtcdTLSSecretNamespace: os.Getenv("ETCD_ENDPOINT_SECRET_NAMESPACE"),
+				EtcdTLSSecretName:      os.Getenv("ETCD_ENDPOINT_SECRET_NAME"),
+			},
+			ApiServer: APIServer{
+				APIServerEndpointURL: os.Getenv("API_SERVER_ENDPOINT_URL"),
+				APIServerSecurePort:  os.Getenv("API_SERVER_SECURE_PORT"),
+			},
+			Scheduler: Scheduler{
+				SchedulerEndpointURL: os.Getenv("SCHEDULER_ENDPOINT_URL"),
+			},
+			ControllerManager: ControllerManager{
+				ControllerManagerEndpointURL: os.Getenv("CONTROLLER_MANAGER_ENDPOINT_URL"),
+			},
 		},
 	}
 }
