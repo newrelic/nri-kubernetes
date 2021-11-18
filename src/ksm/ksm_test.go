942314f62f2e1b5924b71e0971ac8f917f6df07c
Roberto Santalla <roobre@users.noreply.github.com>
2021-11-18 13:44:27 +0100

    Integration test and static main improvements (#263)
    * test: new integration testing backbone
    kubernetes-static main has been migrated to the new backbone as well
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * test/integration: make Version object return mocks
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * test/integration: implement the Asserter
    Asserter is a (hopefully) powerful tool to assert that all metrics defined in a SpecGroup are correctly captured.
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * test/integration: implement helper for building integrations
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * test/asserter: log missing metric namespace
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * test/asserter: make asserter fully thread-safe
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * test/ksm: optimize asserter calls on test parallelization
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * fixup! test/asserter: typos
    
    Signed-off-by: Roberto Santalla <roobre@roobre.es>
    
    * testutil: make LatestVersion rely on the order of AllVersions
    
    * testutil: do not wrap fake.NewSimpleClientset, but rather provide helpers with objects\
    fixup! add namespaces
    
    * testutil/asserter: check if specGroups are empty and fail in that case
    
    * testutil/asserter: make Excluding copy-safe
    
    * test/ksm: share the asserter again
    I've added comments explaining how this works, which will hopefully improve readability.

---
+++ src/ksm/ksm_test.go
@@ -0,0 +1,62 @@
+package ksm_test
+
+// This file holds the integration tests for the KSM package.
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/newrelic/nri-kubernetes/v2/internal/config"
+	"github.com/newrelic/nri-kubernetes/v2/internal/testutil"
+	"github.com/newrelic/nri-kubernetes/v2/src/ksm"
+	ksmClient "github.com/newrelic/nri-kubernetes/v2/src/ksm/client"
+	"github.com/newrelic/nri-kubernetes/v2/src/metric"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestScraper(t *testing.T) {
+	// Create an asserter with the settings that are shared for all test scenarios.
+	asserter := testutil.NewAsserter().
+		Using(metric.KSMSpecs).
+		// TODO(roobre): We should not exclude Optional, pod or hpa metrics. To be tackled in a follow-up PR.
+		ExcludingOptional().
+		Excluding("pod").
+		Excluding("hpa")
+
+	for _, version := range testutil.AllVersions() {
+		t.Run(fmt.Sprintf("for_version_%s", version), func(t *testing.T) {
+			t.Parallel()
+
+			testServer, err := version.Server()
+			if err != nil {
+				t.Fatalf("Cannot create fake KSM server: %v", err)
+			}
+
+			ksmCli, err := ksmClient.New()
+			if err != nil {
+				t.Fatalf("error creating ksm client: %v", err)
+			}
+
+			fakeK8s := fake.NewSimpleClientset(testutil.K8sEverything()...)
+			scraper, err := ksm.NewScraper(&config.Mock{
+				KSM: config.KSM{
+					StaticURL: testServer.KSMEndpoint(),
+				},
+				ClusterName: t.Name(),
+			}, ksm.Providers{
+				K8s: fakeK8s,
+				KSM: ksmCli,
+			})
+
+			i := testutil.NewIntegration(t)
+
+			err = scraper.Run(i)
+			if err != nil {
+				t.Fatalf("running scraper: %v", err)
+			}
+
+			// Call the asserter for the entities of this particular sub-test.
+			asserter.On(i.Entities).Assert(t)
+		})
+	}
+}
