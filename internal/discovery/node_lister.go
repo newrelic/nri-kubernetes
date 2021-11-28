10f048992eaeb01eacece2b5a22d2336e5db68a7
paologallinaharbur <paologallina1992@gmail.com>
2021-11-28 20:28:47 +0100

    fix(imports): improved imports names

--- internal/discovery/node_lister.go
+++ internal/discovery/node_lister.go
@@ -3,11 +3,11 @@ package discovery
 import (
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	v1 "k8s.io/client-go/listers/core/v1"
+	listersv1 "k8s.io/client-go/listers/core/v1"
 )
 
 // NewNodeLister returns a NodeGetter to get nodes with informers.
-func NewNodeLister(client kubernetes.Interface, options ...informers.SharedInformerOption) (v1.NodeLister, chan<- struct{}) {
+func NewNodeLister(client kubernetes.Interface, options ...informers.SharedInformerOption) (listersv1.NodeLister, chan<- struct{}) {
 	stopCh := make(chan struct{})
 
 	factory := informers.NewSharedInformerFactoryWithOptions(client, defaultResyncDuration, options...)
