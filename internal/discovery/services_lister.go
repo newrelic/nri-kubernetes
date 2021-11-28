18306264b00b41ab0fb1a07fce4372fd09231caa
paologallinaharbur <paologallina1992@gmail.com>
2021-11-28 20:28:47 +0100

    fix(imports): improved imports names

--- internal/discovery/services_lister.go
+++ internal/discovery/services_lister.go
@@ -5,13 +5,13 @@ import (
 
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	v1 "k8s.io/client-go/listers/core/v1"
+	listersv1 "k8s.io/client-go/listers/core/v1"
 )
 
 // defaultResyncDuration is an arbitrary value, same used in Prometheus.
 const defaultResyncDuration = 10 * time.Minute
 
-func NewServicesLister(client kubernetes.Interface, options ...informers.SharedInformerOption) (v1.ServiceLister, chan<- struct{}) {
+func NewServicesLister(client kubernetes.Interface, options ...informers.SharedInformerOption) (listersv1.ServiceLister, chan<- struct{}) {
 	stopCh := make(chan struct{})
 
 	factory := informers.NewSharedInformerFactoryWithOptions(client, defaultResyncDuration, options...)
