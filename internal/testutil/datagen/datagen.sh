fa9f13150416223b3d8242922d9f4da630fa848b
Roberto Santalla <roobre@roobre.es>
2021-11-30 15:53:19 +0100

    testutil/datagen: wait for e2e-resources to settle This could be easier to do with helm --wait. However, since we purposefully deploy resources that never reach a stable state, it is not possible do it like that.

--- internal/testutil/datagen/datagen.sh
+++ internal/testutil/datagen/datagen.sh
@@ -124,6 +124,11 @@ function bootstrap() {
         echo "Installing KSM"
         helm dependency update ../../../e2e/charts/ksm > /dev/null
         helm upgrade --install ksm ../../../e2e/charts/ksm -n ksm --create-namespace --wait
+
+        echo "Waiting for E2E resources to settle"
+        kubectl -n $scrapper_namespace wait --for=condition=Ready pod -l app=hpa
+        kubectl -n $scrapper_namespace wait --for=condition=Ready pod -l app=daemonset
+        kubectl -n $scrapper_namespace wait --for=condition=Ready pod -l app=statefulset
     fi
 
     echo "Waiting for scraper pod to be ready"
