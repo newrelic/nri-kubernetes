40315e300ba86a3806c39ecb90a46080d6c84647
paologallinaharbur <paologallina1992@gmail.com>
2021-12-28 17:55:21 +0100

    chore(cleanup): remove unused code

--- src/client/interfaces.go
+++ src/client/interfaces.go
@@ -2,25 +2,7 @@ package client
 
 import (
 	"net/http"
-	"time"
 )
-
-// Discoverer allows discovering the endpoints from different services in the Kubernetes ecosystem.
-type Discoverer interface {
-	Discover(timeout time.Duration) (HTTPClient, error)
-}
-
-// MultiDiscoverer allows for discovery processes that return more
-// than a single HTTP client. For example, in one node we might query
-// more than one KSM instance, so we need two clients.
-type MultiDiscoverer interface {
-	Discover(timeout time.Duration) ([]HTTPClient, error)
-}
-
-// HTTPClient allows to connect to the discovered Kubernetes services
-type HTTPClient interface {
-	HTTPGetter
-}
 
 // HTTPGetter is an interface for HTTP client with, which should provide
 // scheme, port and hostname for the HTTP call.
