8c01d662c2887af76e31b6365f3514bf45a58e81
Roberto Santalla <roobre@roobre.es>
2021-12-29 11:04:56 +0100

    kubelet/test: move exclusions to a helper func

--- src/kubelet/kubelet_test.go
+++ src/kubelet/kubelet_test.go
@@ -28,101 +28,11 @@ import (
 )
 
 func TestScraper(t *testing.T) {
-	// Network metrics are known to be missing on some environments.
-	networkMetrics := []string{"net.rxBytesPerSecond", "net.txBytesPerSecond", "net.errorsPerSecond"}
-	// TODO
-	nodeUtilizationMetrics := []string{"allocatableCpuCoresUtilization", "allocatableMemoryUtilization"}
-
-	// Pods and containers that are not in a running state will not have these metrics.
-	notRunningMetrics := []string{"memoryUsedBytes", "memoryWorkingSetBytes", "cpuUsedCores",
-		"fsAvailableBytes", "fsCapacityBytes", "fsUsedBytes", "fsUsedPercent", "fsInodesFree", "fsInodes",
-		"fsInodesUsed", "containerMemoryMappedFileBytes", "containerID", "containerImageID", "isReady", "podIP"}
-
-	// Utilization metrics will not be present if the corresponding limit/request is not present.
-	utilizationDependencies := map[string][]string{
-		"cpuLimitCores":        {"cpuCoresUtilization"},
-		"cpuRequestedCores":    {"requestedCpuCoresUtilization"},
-		"memoryLimitBytes":     {"memoryUtilization"},
-		"memoryRequestedBytes": {"requestedMemoryUtilization"},
-	}
-
-	limitsRequestsRegex := regexp.MustCompile("(Limit|Requested)(Cores|Bytes)$")
-
 	// Create an asserter with the settings that are shared for all test scenarios.
 	asserter := asserter.New().
 		Silently().
 		Using(metric.KubeletSpecs).
-		Excluding(
-			// Network metrics
-			exclude.Metrics(networkMetrics...),
-
-			// Node utilization  metrics.
-			exclude.Exclude(exclude.Groups("node"), exclude.Metrics(nodeUtilizationMetrics...)),
-			// Exclude limits/requested metrics for nodes, pods and containers
-			exclude.Exclude(
-				exclude.Groups("node", "pod", "container"),
-				func(group string, spec *definition.Spec, e *integration.Entity) bool {
-					return limitsRequestsRegex.MatchString(spec.Name)
-				},
-			),
-			// Exclude metrics that depend on limits when those limits are not set.
-			exclude.Exclude(exclude.Groups("pod", "container"), exclude.Dependent(utilizationDependencies)),
-
-			// Static pods, typically living in kube-system, do not have creation dates.
-			exclude.Exclude(
-				exclude.Groups("pod"),
-				func(_ string, _ *definition.Spec, ent *integration.Entity) bool {
-					return asserter.EntityHas(ent, "namespace", "kube-system")
-				},
-				exclude.Metrics("createdAt", "createdBy", "createdKind", "deploymentName"),
-			),
-
-			// Exclude deploymentName metric for pods not created by a deployment
-			exclude.Exclude(
-				exclude.Groups("pod", "container"),
-				func(_ string, _ *definition.Spec, ent *integration.Entity) bool {
-					return !asserter.EntityHas(ent, "createdKind", "deployment")
-				},
-				exclude.Metrics("createdAt", "createdBy", "createdKind", "deploymentName"),
-			),
-
-			// Exclude metrics known to be missing for pods that are pending.
-			exclude.Exclude(
-				exclude.Groups("pod", "container"),
-				func(group string, _ *definition.Spec, ent *integration.Entity) bool {
-					return !asserter.EntityHas(ent, "status", "running")
-				},
-				exclude.Metrics(notRunningMetrics...),
-			),
-
-			// Reason and message are only present where a pod/container is pending or terminated
-			exclude.Exclude(
-				exclude.Groups("pod", "container"),
-				func(_ string, _ *definition.Spec, ent *integration.Entity) bool {
-					return !asserter.EntityHas(ent, "status", "pending") &&
-						!asserter.EntityHas(ent, "status", "terminated")
-				},
-				exclude.Metrics("reason", "message"),
-			),
-
-			// Fair scheduler metrics are not present sometimes.
-			// TODO: Investigate further why.
-			exclude.Exclude(
-				func(_ string, spec *definition.Spec, _ *integration.Entity) bool {
-					return strings.HasPrefix(spec.Name, "containerCpuCfs")
-				},
-			),
-
-			// Exclude PVC metrics for volumes that are not named "pv"
-			exclude.Exclude(
-				exclude.Groups("volume"),
-				func(_ string, spec *definition.Spec, e *integration.Entity) bool {
-					return e.Metadata.Name != "pv" && strings.HasPrefix(spec.Name, "pvc")
-				},
-			),
-			// Optional metrics.
-			//exclude.Optional(),
-		)
+		Excluding(kubeletExclusions()...)
 
 	for _, v := range testutil.AllVersions() {
 		// Make a copy of the version variable to use it concurrently
@@ -166,5 +76,102 @@ func TestScraper(t *testing.T) {
 			// Call the asserter for the entities of this particular sub-test.
 			asserter.On(i.Entities).Assert(t)
 		})
+	}
+}
+
+// kubeletExclusions is a helper that returns all the exclusions needed to assert the kubelet metrics without getting
+// false negatives.
+func kubeletExclusions() []exclude.Func {
+	// Network metrics are known to be missing on some environments.
+	networkMetrics := []string{"net.rxBytesPerSecond", "net.txBytesPerSecond", "net.errorsPerSecond"}
+
+	// TODO: Unclear why we need to exclude node utilization metrics.
+	nodeUtilizationMetrics := []string{"allocatableCpuCoresUtilization", "allocatableMemoryUtilization"}
+
+	// Pods and containers that are not in a running state will not have these metrics.
+	notRunningMetrics := []string{"memoryUsedBytes", "memoryWorkingSetBytes", "cpuUsedCores",
+		"fsAvailableBytes", "fsCapacityBytes", "fsUsedBytes", "fsUsedPercent", "fsInodesFree", "fsInodes",
+		"fsInodesUsed", "containerMemoryMappedFileBytes", "containerID", "containerImageID", "isReady", "podIP"}
+
+	// Utilization metrics will not be present if the corresponding limit/request is not present.
+	utilizationDependencies := map[string][]string{
+		"cpuLimitCores":        {"cpuCoresUtilization"},
+		"cpuRequestedCores":    {"requestedCpuCoresUtilization"},
+		"memoryLimitBytes":     {"memoryUtilization"},
+		"memoryRequestedBytes": {"requestedMemoryUtilization"},
+	}
+
+	// Regex to match limits/requests for CPU and Memory.
+	limitsRequestsRegex := regexp.MustCompile("(Limit|Requested)(Cores|Bytes)$")
+
+	return []exclude.Func{
+		// Network metrics
+		exclude.Metrics(networkMetrics...),
+
+		// Node utilization  metrics.
+		exclude.Exclude(exclude.Groups("node"), exclude.Metrics(nodeUtilizationMetrics...)),
+
+		// Exclude limits/requested metrics for nodes, pods and containers
+		exclude.Exclude(
+			exclude.Groups("node", "pod", "container"),
+			func(group string, spec *definition.Spec, e *integration.Entity) bool {
+				return limitsRequestsRegex.MatchString(spec.Name)
+			},
+		),
+		// Exclude metrics that depend on limits when those limits are not set.
+		exclude.Exclude(exclude.Groups("pod", "container"), exclude.Dependent(utilizationDependencies)),
+
+		// Static pods, typically living in kube-system, do not have creation dates.
+		exclude.Exclude(
+			exclude.Groups("pod"),
+			func(_ string, _ *definition.Spec, ent *integration.Entity) bool {
+				return asserter.EntityHas(ent, "namespace", "kube-system")
+			},
+			exclude.Metrics("createdAt", "createdBy", "createdKind", "deploymentName"),
+		),
+
+		// Exclude deploymentName metric for pods not created by a deployment
+		exclude.Exclude(
+			exclude.Groups("pod", "container"),
+			func(_ string, _ *definition.Spec, ent *integration.Entity) bool {
+				return !asserter.EntityHas(ent, "createdKind", "deployment")
+			},
+			exclude.Metrics("createdAt", "createdBy", "createdKind", "deploymentName"),
+		),
+
+		// Exclude metrics known to be missing for pods that are pending.
+		exclude.Exclude(
+			exclude.Groups("pod", "container"),
+			func(group string, _ *definition.Spec, ent *integration.Entity) bool {
+				return !asserter.EntityHas(ent, "status", "running")
+			},
+			exclude.Metrics(notRunningMetrics...),
+		),
+
+		// Reason and message are only present where a pod/container is pending or terminated
+		exclude.Exclude(
+			exclude.Groups("pod", "container"),
+			func(_ string, _ *definition.Spec, ent *integration.Entity) bool {
+				return !asserter.EntityHas(ent, "status", "pending") &&
+					!asserter.EntityHas(ent, "status", "terminated")
+			},
+			exclude.Metrics("reason", "message"),
+		),
+
+		// Fair scheduler metrics are not present sometimes.
+		// TODO: Investigate further why.
+		exclude.Exclude(
+			func(_ string, spec *definition.Spec, _ *integration.Entity) bool {
+				return strings.HasPrefix(spec.Name, "containerCpuCfs")
+			},
+		),
+
+		// Exclude PVC metrics for volumes that are not named "pv"
+		exclude.Exclude(
+			exclude.Groups("volume"),
+			func(_ string, spec *definition.Spec, e *integration.Entity) bool {
+				return e.Metadata.Name != "pv" && strings.HasPrefix(spec.Name, "pvc")
+			},
+		),
 	}
 }
