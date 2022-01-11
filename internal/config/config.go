e937ff9733a4dc485ff2c55ce9ade4db1f53feb3
Roberto Santalla <roobre@users.noreply.github.com>
2022-01-11 09:58:19 +0100

    ksm: wait for some time for endpoints to appear before erroring (#312)
    * ksm: wait for some time for endpoints to appear before erroring
    A new `EndpointsDiscoverer` implementation, `EndpointsDiscovererWithTimeout` has been added. EndpointsDiscovererWithTimeout wraps another EndpointsDiscoverer adding a simple retry logic on top.

--- internal/config/config.go
+++ internal/config/config.go
@@ -35,6 +35,12 @@ type KSM struct {
 	Namespace   string `mapstructure:"namespace"`
 	Distributed bool   `mapstructure:"distributed"`
 	Enabled     bool   `mapstructure:"enabled"`
+	Discovery   struct {
+		// Wait BackoffDelay between discovery attempts.
+		BackoffDelay time.Duration `mapstructure:"backoffDelay"`
+		// Give up discovery and fail if Timeout has passed since first attempt.
+		Timeout time.Duration `mapstructure:"timeout"`
+	} `mapstructure:"discovery"`
 }
 
 type Kubelet struct {
@@ -92,6 +98,8 @@ func LoadConfig(filePath string, fileName string) (*Config, error) {
 	v.SetDefault("nodeName", "node")
 	v.SetDefault("nodeIP", "node")
 	v.SetDefault("httpServerPort", 0)
+	v.SetDefault("ksm.discovery.backoffDelay", 7*time.Second)
+	v.SetDefault("ksm.discovery.timeout", 60*time.Second)
 
 	v.SetEnvPrefix("NRI_KUBERNETES")
 	v.AutomaticEnv()
