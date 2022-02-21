d84cea95562076e1ba213bb0ada46985e1030bb2
Roberto Santalla <roobre@users.noreply.github.com>
2022-02-21 14:45:51 +0100

    integration: allow to specify a custom sink (#362)

--- src/integration/wrapper.go
+++ src/integration/wrapper.go
@@ -2,18 +2,21 @@ package integration
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 	"strconv"
 	"time"
 
 	sdk "github.com/newrelic/infra-integrations-sdk/integration"
+	"github.com/sethgrid/pester"
+	log "github.com/sirupsen/logrus"
+
 	"github.com/newrelic/nri-kubernetes/v3/internal/config"
 	"github.com/newrelic/nri-kubernetes/v3/internal/logutil"
 	"github.com/newrelic/nri-kubernetes/v3/internal/storer"
 	"github.com/newrelic/nri-kubernetes/v3/src/integration/prober"
 	"github.com/newrelic/nri-kubernetes/v3/src/integration/sink"
-	"github.com/sethgrid/pester"
-	log "github.com/sirupsen/logrus"
 )
 
 const defaultProbeTimeout = 90 * time.Second
@@ -28,6 +31,7 @@ type Wrapper struct {
 	metadata       Metadata
 	probeTimeout   time.Duration
 	probeBackoff   time.Duration
+	sink           io.Writer
 }
 
 // OptionFunc is an option func for the Wrapper.
@@ -48,6 +52,42 @@ func WithMetadata(metadata Metadata) OptionFunc {
 	}
 }
 
+// WithHTTPSink configures the wrapper to use an HTTP Sink for metrics.
+// If this option is not specified, Wrapper will configure the integration.Integration to sink metrics to stdout.
+func WithHTTPSink(sinkConfig config.HTTPSink) OptionFunc {
+	return func(iw *Wrapper) error {
+		hostPort := net.JoinHostPort(sink.DefaultAgentForwarderhost, strconv.Itoa(sinkConfig.Port))
+
+		prober := prober.New(iw.probeTimeout, iw.probeBackoff)
+		iw.logger.Info("Waiting for agent container to be ready...")
+
+		err := prober.Probe(fmt.Sprintf("http://%s%s", hostPort, agentReadyPath))
+		if err != nil {
+			return fmt.Errorf("timeout waiting for agent: %w", err)
+		}
+
+		c := pester.New()
+		c.Backoff = pester.LinearBackoff
+		c.MaxRetries = sinkConfig.Retries
+		c.Timeout = sinkConfig.Timeout
+		c.LogHook = func(e pester.ErrEntry) {
+			// LogHook is invoked only when an error happens
+			iw.logger.Warnf("Error sending data to agent sink: %q", e)
+		}
+
+		h, err := sink.New(sink.HTTPSinkOptions{
+			URL:    fmt.Sprintf("http://%s%s", hostPort, sink.DefaultAgentForwarderPath),
+			Client: c,
+		})
+		if err != nil {
+			return fmt.Errorf("creating HTTP Sink: %w", err)
+		}
+
+		iw.sink = h
+		return nil
+	}
+}
+
 // Metadata contains the integration name and version that is passed down to the integration SDK.
 type Metadata struct {
 	Name    string
@@ -58,6 +98,7 @@ type Metadata struct {
 func NewWrapper(opts ...OptionFunc) (*Wrapper, error) {
 	intgr := &Wrapper{
 		logger:       logutil.Discard,
+		sink:         os.Stdout,
 		probeTimeout: defaultProbeTimeout,
 		probeBackoff: defaultProbeBackoff,
 	}
@@ -74,34 +115,7 @@ func NewWrapper(opts ...OptionFunc) (*Wrapper, error) {
 
 // Integration returns a sdk.Integration, configured to output data to the specified agent.
 // Integration will block and wait until the specified server is ready, up to a maximum timeout.
-func (iw *Wrapper) Integration(sinkConfig config.HTTPSink) (*sdk.Integration, error) {
-	hostPort := net.JoinHostPort(sink.DefaultAgentForwarderhost, strconv.Itoa(sinkConfig.Port))
-
-	prober := prober.New(iw.probeTimeout, iw.probeBackoff)
-	iw.logger.Info("Waiting for agent container to be ready...")
-
-	err := prober.Probe(fmt.Sprintf("http://%s%s", hostPort, agentReadyPath))
-	if err != nil {
-		return nil, fmt.Errorf("timeout waiting for agent: %w", err)
-	}
-
-	c := pester.New()
-	c.Backoff = pester.LinearBackoff
-	c.MaxRetries = sinkConfig.Retries
-	c.Timeout = sinkConfig.Timeout
-	c.LogHook = func(e pester.ErrEntry) {
-		// LogHook is invoked only when an error happens
-		iw.logger.Warnf("Error sending data to agent sink: %q", e)
-	}
-
-	h, err := sink.New(sink.HTTPSinkOptions{
-		URL:    fmt.Sprintf("http://%s%s", hostPort, sink.DefaultAgentForwarderPath),
-		Client: c,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("creating HTTPSink: %w", err)
-	}
-
+func (iw *Wrapper) Integration() (*sdk.Integration, error) {
 	cache := storer.NewInMemoryStore(storer.DefaultTTL, storer.DefaultInterval, iw.logger)
-	return sdk.New(iw.metadata.Name, iw.metadata.Version, sdk.Writer(h), sdk.Storer(cache))
+	return sdk.New(iw.metadata.Name, iw.metadata.Version, sdk.Writer(iw.sink), sdk.Storer(cache))
 }
