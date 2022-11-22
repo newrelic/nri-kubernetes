65c11564ad5aae1b114fdac74f44ba23e493ca88
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-11-22 17:27:22 +0100

    chore: improve comments

--- src/definition/guessfunc.go
+++ src/definition/guessfunc.go
@@ -6,6 +6,7 @@ import (
 )
 
 // K8sMetricSetTypeGuesser is the metric set type guesser for k8s integrations.
+// It composes the 'event_type' value using the group label defined in the corresponding specs.
 func K8sMetricSetTypeGuesser(groupLabel string) (string, error) {
 	var sampleName string
 	for _, s := range strings.Split(groupLabel, "-") {
