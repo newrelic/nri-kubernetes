a247670102b20e1a959ef85354311b4221df4e3c
Hannah Troisi <htroisi@pixielabs.ai>
2022-12-05 08:13:30 -0800

    Extend integration tests to support k8s 1.25 (#585)
    * Extend Integration tests to support k8s 1.25
    
    Signed-off-by: Hannah Troisi <htroisi@pixielabs.ai>

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -35,6 +35,7 @@ const (
 	Testdata122 = "1_22"
 	Testdata123 = "1_23"
 	Testdata124 = "1_24"
+	Testdata125 = "1_25"
 )
 
 // AllVersions returns a list of versions we have test data for.
@@ -48,6 +49,7 @@ func AllVersions() []Version {
 		Testdata122,
 		Testdata123,
 		Testdata124,
+		Testdata125,
 	}
 }
 
