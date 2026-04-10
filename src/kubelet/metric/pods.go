272df518e30a95a197958aab899fbe175705428e
James McGivern <jmcgivern@newrelic.com>
2026-04-10 03:23:29 -0500

    only get pod resource spec data if it exists

--- src/kubelet/metric/pods.go
+++ src/kubelet/metric/pods.go
@@ -368,20 +368,22 @@ func (podsFetcher *PodsFetcher) fetchPodData(pod *v1.Pod) definition.RawMetrics 
 		metrics["priority"] = *pod.Spec.Priority
 	}
 
-	if v, ok := pod.Spec.Resources.Requests[v1.ResourceCPU]; ok {
-		metrics["cpuRequestedCores"] = v.MilliValue()
-	}
+	if pod.Spec.Resources != nil {
+		if v, ok := pod.Spec.Resources.Requests[v1.ResourceCPU]; ok {
+			metrics["cpuRequestedCores"] = v.MilliValue()
+		}
 
-	if v, ok := pod.Spec.Resources.Limits[v1.ResourceCPU]; ok {
-		metrics["cpuLimitCores"] = v.MilliValue()
-	}
+		if v, ok := pod.Spec.Resources.Limits[v1.ResourceCPU]; ok {
+			metrics["cpuLimitCores"] = v.MilliValue()
+		}
 
-	if v, ok := pod.Spec.Resources.Requests[v1.ResourceMemory]; ok {
-		metrics["memoryRequestedBytes"] = v.Value()
-	}
+		if v, ok := pod.Spec.Resources.Requests[v1.ResourceMemory]; ok {
+			metrics["memoryRequestedBytes"] = v.Value()
+		}
 
-	if v, ok := pod.Spec.Resources.Limits[v1.ResourceMemory]; ok {
-		metrics["memoryLimitBytes"] = v.Value()
+		if v, ok := pod.Spec.Resources.Limits[v1.ResourceMemory]; ok {
+			metrics["memoryLimitBytes"] = v.Value()
+		}
 	}
 
 	labels := podLabels(pod)
